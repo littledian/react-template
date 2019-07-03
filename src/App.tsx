2b60983be2030490e59c214e608652a936cd0c0d
littledian <1197434548@qq.com>
2019-07-03 23:37:16 +0800

    feat: update template

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
-import React, { Component, ReactNode } from 'react'
+import React from 'react'
 import style from './App.module.scss'
 
-export interface AppProps {}
-
-export interface AppState {}
-
-export default class App extends Component<AppProps, AppState> {
-  render (): ReactNode {
-    return (
-      <div className={style.root}>
-        React Template
-      </div>
-    )
-  }
+export default function App () {
+  return (
+    <div className={style.root}>
+      React Template
+    </div>
+  )
 }

cf1bd6661941da6c036abf65adfa85d3c815cafb
littledian <1197434548@qq.com>
2020-08-30 21:35:27 +0800

    build: update build script

--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+# install dependencies
+echo start install node_modules
+yarn
+
+# build assets
+yarn build
+
+# build images
+
 timestamp=`date "+%Y%m%d%H%M%S"`
 prefix="react-template"
 containerName="react-template";
@@ -7,11 +17,15 @@ tag="$prefix:$timestamp"
 
 docker build -t "$tag" .
 
+# stop current container
+
 have=$(docker inspect --format='{{.Name}}' $(docker ps -aq) |grep $containerName  | cut -d"/" -f2)
 if [[ "$have" == "$containerName" ]]; then
   docker container stop $containerName
   docker container rm $containerName
 fi
+
+# remove prev images
 
 tags=$(docker images | grep $prefix | awk '{print $2}')
 for item in $tags
@@ -21,4 +35,5 @@ do
   fi
 done
 
+# start current image
 docker run -d -p "$port:$port" --name $containerName --restart always "$tag"
